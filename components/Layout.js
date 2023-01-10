4207b912094bf7398f46263d64df130f680f58bc
iamluqmaan <obalolaluqman16@gmail.com>
2023-01-10 11:24:55 -0600

    initial

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,8 @@ export default function Layout({ children }) {
         <Link href="/">
           <a>
             <h1>
-              <span>Just Add</span>
+
+              <span>Just Add</span> 
               <span>Marmite</span>
             </h1>
             <h2>Spread The Joy</h2>
